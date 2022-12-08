c5d8a22c53d00fb3d9ab1efcf876664ddd6ec8c0
Andrew Ouzts <pezzor@gmail.com>
2022-12-07 22:07:34 -0600

    Add project files.

---
+++ AppLauncher/AppLauncher.cpp
@@ -0,0 +1,12 @@
+﻿// AppLauncher.cpp : Defines the entry point for the application.
+//
+
+#include "AppLauncher.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
